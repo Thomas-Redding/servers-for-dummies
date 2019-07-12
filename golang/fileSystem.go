745a1920d95088d18ea6eba94cf901520401f10a
Thomas Redding <thomasredding@Thomass-MacBook-Pro-3.local>
2019-07-11 22:53:37 -0700

    Add support for Google Cloud Storage.

---
+++ golang/fileSystem.go
@@ -0,0 +1,64 @@
+
+package main
+
+import (
+  "errors"
+  "fmt"
+  "io/ioutil"
+
+  "cloud.google.com/go/storage"
+  "golang.org/x/net/context"
+  "google.golang.org/appengine/log"
+)
+
+type FileSystem struct {
+  _ctx context.Context
+  _client *storage.Client
+  _bucketName string
+  _bucketHandle *storage.BucketHandle
+}
+
+func connectToFileSystem(ctx context.Context, client *storage.Client, bucketName string) *FileSystem {
+  bucketHandle := client.Bucket(bucketName)
+  return &FileSystem{_ctx: ctx, _client: client, _bucketName: bucketName, _bucketHandle: bucketHandle}
+}
+
+/*
+ Overwrite a file at `filePath` with `newContents`. If no file exists, create a new one.
+ */
+func (fileSystem *FileSystem) write(filePath string, newContents []byte) error {
+  wc := fileSystem._bucketHandle.Object(filePath).NewWriter(fileSystem._ctx)
+  wc.ContentType = "text/plain"
+  wc.Metadata = map[string]string{
+    "x-goog-meta-foo": "foo",
+    "x-goog-meta-bar": "bar",
+  }
+  if _, err := wc.Write(newContents); err != nil {
+    errorString := fmt.Sprintf("createFile: unable to write data to bucket %q, file %q: %v", fileSystem._bucketName, filePath, err)
+    log.Errorf(fileSystem._ctx, errorString)
+    return errors.New(errorString)
+  }
+  if err := wc.Close(); err != nil {
+    errorString := fmt.Sprintf("createFile: unable to close bucket %q, file %q: %v", fileSystem._bucketName, filePath, err)
+    log.Errorf(fileSystem._ctx, errorString)
+    return errors.New(errorString)
+  }
+  return nil
+}
+
+func (fileSystem *FileSystem) read(fileName string) ([]byte, error) {
+  rc, err := fileSystem._bucketHandle.Object(fileName).NewReader(fileSystem._ctx)
+  if err != nil {
+    errorString := fmt.Sprintf("readFile: unable to open file from bucket %q, file %q: %v", fileSystem._bucketName, fileName, err)
+    log.Errorf(fileSystem._ctx, errorString)
+    return nil, errors.New(errorString)
+  }
+  defer rc.Close()
+  slurp, err := ioutil.ReadAll(rc)
+  if err != nil {
+    errorString := fmt.Sprintf("readFile: unable to read data from bucket %q, file %q: %v", fileSystem._bucketName, fileName, err)
+    log.Errorf(fileSystem._ctx, errorString)
+    return nil, errors.New(errorString)
+  }
+  return slurp, nil
+}
--- golang/main.go
+++ golang/main.go
@@ -2,8 +2,13 @@
 package main
 
 import (
-  "google.golang.org/appengine"
+  "fmt"
   "net/http"
+
+  "cloud.google.com/go/storage"
+  "google.golang.org/appengine"
+  "google.golang.org/appengine/file"
+  "google.golang.org/appengine/log"
 )
 
 func main() {
@@ -12,6 +17,29 @@ func main() {
 }
 
 func handle(writer http.ResponseWriter, request *http.Request) {
+  ctx := appengine.NewContext(request)
+  bucketName, err := file.DefaultBucketName(ctx)
+  if err != nil {
+    log.Errorf(ctx, "failed to get default GCS bucket name: %v", err)
+  }
   writer.Header().Set("Content-type", "text/plain")
   writer.Write([]byte("Hello World (Golang)!\n" + request.URL.Path))
+
+  client, err := storage.NewClient(ctx)
+  if err != nil {
+    log.Errorf(ctx, "failed to create client: %v", err)
+    return
+  }
+  defer client.Close()
+
+  fileSystem := connectToFileSystem(ctx, client, bucketName)
+
+  writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+  fmt.Fprintf(writer, "Demo GCS Application running from Version: %v\n", appengine.VersionID(ctx))
+  fmt.Fprintf(writer, "Using bucket name: %v\n\n", bucketName)
+
+  fileSystem.write("demo-testfile-go", []byte("lorem lorem 123"))
+  fileData, _ := fileSystem.read("demo-testfile-go")
+  fmt.Fprintf(writer, "File data: %v\n\n", string(fileData))
+  fmt.Fprintf(writer, "temp: %v\n\n", string(fileData))
 }
