f92ff309954c6eb714ef7bfd3952f61a45dab23e
Thomas Redding <thomasredding@Thomass-MacBook-Pro-3.local>
2019-07-11 23:46:06 -0700

    Added broken Python.

--- python/main.py
+++ python/main.py
@@ -1,10 +1,19 @@
+import os
+import lib.cloudstorage
+from google.appengine.api import app_identity
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello World (Python)!')
+  def get(self):
+    bucketName = os.environ.get('BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
 
-app = webapp2.WSGIApplication([
-    ('/*', MainPage),
-], debug=True)
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.write('Hello World (Python)!')
+
+  def write(self, bucketName, filePath, data):
+    filename = '/' + bucketName + '/' + filePath
+    writeRetryParams = cloudstorage.RetryParams(backoff_factor=1.1)
+    with cloudstorage.open(filename, 'w', content_type='text/plain', options={'x-goog-meta-foo': 'foo', 'x-goog-meta-bar': 'bar'}, retry_params=writeRetryParams) as cloudstorageFile:
+      cloudstorageFile.write(data)
+
+app = webapp2.WSGIApplication([('/*', MainPage)], debug=True)
